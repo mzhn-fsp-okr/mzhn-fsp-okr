1bf5c869d757bb0bccb2cc478181f6a7aa9bb9f5
StarPandaBeg <kshmurakov@mail.ru>
2024-11-22 22:49:34 +0300

    fix: improve responsive design

--- frontend-landing/src/app/(main)/layout.tsx
+++ frontend-landing/src/app/(main)/layout.tsx
@@ -7,12 +7,12 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <section className="grid min-h-screen w-full divide-y divide-white/5 py-4 sm:grid-cols-[100px_1fr_100px] sm:grid-rows-[auto_1fr_auto]">
-      <PageHeader className="col-start-2 row-start-1 border-x" />
-      <main className="col-start-2 row-start-2 border-x border-white/5 py-4">
+    <section className="grid min-h-screen w-full grid-cols-1 grid-rows-[80px_1fr_auto] divide-y divide-white/5 py-4 md:grid-cols-[100px_1fr_100px] md:grid-rows-[auto_1fr_auto]">
+      <PageHeader className="row-start-1 border-x md:col-start-2" />
+      <main className="row-start-2 border-x border-white/5 py-4 md:col-start-2">
         {children}
       </main>
-      <PageFooter className="col-start-2 row-start-3 border-x border-white/5" />
+      <PageFooter className="row-start-3 border-x border-white/5 md:col-start-2" />
     </section>
   );
 }
--- frontend-landing/src/app/(main)/page.tsx
+++ frontend-landing/src/app/(main)/page.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/components/ui/badge";
 import {
   Carousel,
   CarouselContent,
@@ -5,15 +6,16 @@ import {
   CarouselThumbnailItemAlt,
   CarouselThumbnails,
 } from "@/components/ui/carousel";
-
-import banner1 from "@/assets/banners/banner-1.png";
-import { Badge } from "@/components/ui/badge";
 import { Dumbbell, Medal, Newspaper, Trophy } from "lucide-react";
 import Image from "next/image";
+
+import banner1 from "@/assets/banners/banner-1.png";
+import banner2 from "@/assets/banners/banner-2.png";
 
 export default function Home() {
   return (
     <section className="space-y-8">
+      123
       <div>
         <Carousel className="w-full text-white">
           <CarouselContent>
@@ -28,7 +30,7 @@ export default function Home() {
             </CarouselItem>
             <CarouselItem>
               <Image
-                src={banner1}
+                src={banner2}
                 alt=""
                 width={768}
                 height={300}
@@ -46,7 +48,7 @@ export default function Home() {
         <h1 className="text-2xl font-bold text-white">События</h1>
         <Carousel className="w-full text-white">
           <CarouselContent className="ml-0 gap-2">
-            <CarouselItem className="relative basis-1/3 rounded bg-sky-600 px-4 py-8">
+            <CarouselItem className="relative rounded bg-sky-600 px-4 py-8 md:basis-1/3">
               <div>
                 <p className="text-sm opacity-85">05-08 декабря</p>
                 <h1 className="text-lg font-bold">Кубок России</h1>
@@ -56,7 +58,7 @@ export default function Home() {
                 <Dumbbell className="size-16 text-white/15" />
               </div>
             </CarouselItem>
-            <CarouselItem className="relative basis-1/3 rounded bg-sky-600 px-4 py-8">
+            <CarouselItem className="relative rounded bg-sky-600 px-4 py-8 md:basis-1/3">
               <div>
                 <p className="text-sm opacity-85">05-08 декабря</p>
                 <h1 className="text-lg font-bold">Кубок России</h1>
@@ -66,7 +68,7 @@ export default function Home() {
                 <Dumbbell className="size-16 text-white/15" />
               </div>
             </CarouselItem>
-            <CarouselItem className="relative basis-1/3 rounded bg-sky-600 px-4 py-8">
+            <CarouselItem className="relative rounded bg-sky-600 px-4 py-8 md:basis-1/3">
               <div>
                 <p className="text-sm opacity-85">05-08 декабря</p>
                 <h1 className="text-lg font-bold">Кубок России</h1>
@@ -83,7 +85,7 @@ export default function Home() {
         <h1 className="text-2xl font-bold text-white">Спорт в СМИ</h1>
         <Carousel className="w-full text-white">
           <CarouselContent className="gap-2">
-            <CarouselItem className="relative basis-1/4 space-y-2">
+            <CarouselItem className="relative space-y-2 md:basis-1/4">
               <div className="group flex h-32 w-full items-center justify-center rounded-none bg-[#000007]">
                 <Newspaper className="transition-all group-hover:scale-150" />
               </div>
@@ -96,7 +98,7 @@ export default function Home() {
                 Читать далее
               </a>
             </CarouselItem>
-            <CarouselItem className="relative basis-1/4 space-y-2">
+            <CarouselItem className="relative space-y-2 md:basis-1/4">
               <div className="group flex h-32 w-full items-center justify-center rounded-none bg-[#ff9900]">
                 <Dumbbell className="transition-all group-hover:scale-150" />
               </div>
@@ -109,7 +111,7 @@ export default function Home() {
                 Читать далее
               </a>
             </CarouselItem>
-            <CarouselItem className="relative basis-1/4 space-y-2">
+            <CarouselItem className="relative space-y-2 md:basis-1/4">
               <div className="group flex h-32 w-full items-center justify-center rounded-none bg-[#221f73]">
                 <Trophy className="transition-all group-hover:scale-150" />
               </div>
@@ -122,7 +124,7 @@ export default function Home() {
                 Читать далее
               </a>
             </CarouselItem>
-            <CarouselItem className="relative basis-1/4 space-y-2">
+            <CarouselItem className="relative space-y-2 md:basis-1/4">
               <div className="group flex h-32 w-full items-center justify-center rounded-none bg-[#262053]">
                 <Medal className="transition-all group-hover:scale-150" />
               </div>
