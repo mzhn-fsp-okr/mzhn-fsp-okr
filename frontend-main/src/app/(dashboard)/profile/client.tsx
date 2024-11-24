d69ed06b50cc303463943b42e278d916ea8a7138
StarPandaBeg <kshmurakov@mail.ru>
2024-11-24 08:02:06 +0300

    wip

--- frontend-main/src/app/(dashboard)/profile/client.tsx
+++ frontend-main/src/app/(dashboard)/profile/client.tsx
@@ -4,7 +4,7 @@ import { getAvatar, getFullName, User } from "@/api/user";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Send } from "lucide-react";
+import { Bell, Mail, Send } from "lucide-react";
 import TelegramDialog from "./tg-dialog";
 
 export default function PageContent({
@@ -32,7 +32,7 @@ export default function PageContent({
             <TabsTrigger value="notifications">Уведомления</TabsTrigger>
           </TabsList>
           <TabsContent value="notifications">
-            <main className="flex">
+            <main className="flex flex-col gap-2 md:flex-row">
               <div className="flex items-center gap-10 rounded border px-8 py-4">
                 <Send className="size-10 text-blue-500" />
                 <div className="flex flex-col gap-2">
@@ -42,6 +42,20 @@ export default function PageContent({
                   ) : (
                     <TelegramDialog />
                   )}
+                </div>
+              </div>
+              <div className="flex items-center gap-10 rounded border px-8 py-4">
+                <Mail className="size-10 text-blue-500" />
+                <div className="flex flex-col gap-2">
+                  <p className="text-lg font-bold">Эл. почта</p>
+                  <Button disabled>Пока недоступно</Button>
+                </div>
+              </div>
+              <div className="flex items-center gap-10 rounded border px-8 py-4">
+                <Bell className="size-10 text-blue-500" />
+                <div className="flex flex-col gap-2">
+                  <p className="text-lg font-bold">Web-уведомления</p>
+                  <Button disabled>Пока недоступно</Button>
                 </div>
               </div>
             </main>
